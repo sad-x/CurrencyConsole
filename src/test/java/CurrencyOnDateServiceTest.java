0c432adadf7e34c42bfb2276a813b5e948313ca8
alex <alex.shamess@yandex.ru>
2021-10-18 16:12:53 +0300

    minor changes

--- src/test/java/CurrencyOnDateServiceTest.java
+++ src/test/java/CurrencyOnDateServiceTest.java
@@ -5,14 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class CurrencyOnDateServiceTest {
-
-    UserInput testUserInput;
-    @BeforeAll
-    public void setUp() {
-        UserInput testUserInput = new UserInput(LocalDate.parse("2010-10-10"), "USD");
-    }
     @Test
     public void getCurrencyTest() {
+        UserInput testUserInput = new UserInput(LocalDate.parse("2010-10-10"), "USD");
         double result = 29.9086;
         Assertions.assertEquals(result, CurrencyOnDateService.getCurrency(testUserInput)[0]);
     }
